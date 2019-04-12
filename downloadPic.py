83c21b947469bc0e2ed35b778b6e15ee8af29313
master <1649601714@qq.com>
2019-04-12 11:41:44 +0800

    Initial commit

---
+++ downloadPic.py
@@ -0,0 +1,82 @@
+import json
+from meiziSpider import get_response
+import os
+from lxml import etree
+from time import sleep
+
+def read_json_file(filename):
+    '''读取json文件数据'''
+    with open(filename) as f:
+        data = f.read()
+        data = json.loads(data)
+    return data
+
+def create_dictionary(name):
+    '''创建文件夹'''
+    try:
+        os.mkdir(name)
+    except Exception as e:
+        print(e)
+
+def download_picture(name,url):
+    '''下载图片'''
+    # 设置headers请求头，获得下载图片权限
+    headers = {
+        'Referer': 'https://www.mzitu.com/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+    }
+    img_content = get_response(url,headers=headers).content
+    try:
+        print('Pictures/'+name+'/'+name+url[-8:])
+        with open('Pictures/'+name+'/'+name+url[-8:],'wb') as f:
+            f.write(img_content)
+    except Exception as e:
+        print(e)
+
+def get_picture_url(name,url):
+    '''获取图片'''
+    response = get_response(url)
+    html = etree.HTML(response.text)
+    # 获取图片url  url在class="main-image"的div标签下的img的src属性
+    img_url = html.xpath('//div[@class="main-image"]//img/@src')[0]
+    # 下载图片到已创建文件夹下
+    download_picture(name,url=img_url)
+    try:
+        # 获取下一页
+        next_page_url = html.xpath('//a[./span/text()="下一页»"]/@href')[0]
+        while next_page_url:
+            print(next_page_url)
+            response = get_response(next_page_url)
+            html = etree.HTML(response.text)
+            img_url = html.xpath('//div[@class="main-image"]//img/@src')[0]
+            download_picture(name, url=img_url)
+            # 设置延时
+            sleep(1)
+            # 获取下一页
+            next_page_url = html.xpath('//a[./span/text()="下一页»"]/@href')[0]
+    except Exception as e:
+        print(e,'该套图保存结束')
+
+
+def run():
+    '''下载图片
+    1.读取json文件数据
+    2.循环数据
+        根据name属性创建文件夹
+        请求套图url地址获取response响应并获取图片url地址
+        下载图片
+        循环获取下一页并下载图片
+    '''
+    # 1.读取json文件数据
+    data = read_json_file(filename='meizi.json')
+    # 创建Picture文件夹
+    create_dictionary(name='Pictures')
+    # 2.循环数据
+    for d in data:
+        # 根据name属性创建文件夹
+        create_dictionary(name='Pictures/'+d['name'])
+        # 循环套图内页面 获取url地址 并下载图片
+        get_picture_url(name=d['name'],url=d['url'])
+
+if __name__ == '__main__':
+    run()
---
+++ meiziSpider.py
@@ -0,0 +1,88 @@
+import requests
+from lxml import etree
+from time import sleep
+import json
+
+def get_response(url,headers=None):
+    '''获取response响应'''
+    response = requests.get(url,headers=headers)
+    return response
+
+def parse_html1(response):
+    '''解析response内容获取套图名称及url地址'''
+    html = etree.HTML(response.text)
+    # 用于保存该页面套图名称及url地址
+    page_img_urls = []
+    # 套图名称及url地址保存在 class="postlist" 的div标签下的 ul标签下 li标签内
+    page_img_content = html.xpath('//div[@class="postlist"]//ul//li')
+    for img_content in page_img_content:
+        # 用于保存单个套图名称及url地址
+        img_info = {}
+        # 套图名称在li标签下第一个a标签下的img标签的alt属性
+        img_info['name'] = img_content.xpath('./a[1]/img/@alt')[0]
+        # 套图url在li标签下的第一个a标签的href属性
+        img_info['url'] = img_content.xpath('./a[1]/@href')[0]
+        page_img_urls.append(img_info)
+    print(page_img_urls)
+    return page_img_urls
+
+def get_next_page_url(response):
+    '''获取下一页url地址'''
+    html = etree.HTML(response.text)
+    next_page_url = html.xpath('//a[text()="下一页»"]/@href')
+    # 判断下一页是否存在
+    if len(next_page_url)>0:
+        return next_page_url[0]
+    else:
+        return None
+
+def save_to_file(filename,data):
+    '''保存数据到json文件'''
+    data = json.dumps(data)
+    filename = filename+'.json'
+    with open(filename,'w') as f:
+        print('正在保存{}'.format(filename))
+        f.write(data)
+    print('保存完成')
+
+
+def run():
+    '''爬虫爬取妹子图图片
+    1.获取首页response响应内容
+    2.解析内容获取套图名称及url地址
+    3.获取下一页url地址
+    4.循环获取下一页response响应内容
+      解析内容获取套图名称及url地址
+    5.将所获得的套图名称及url地址存入json文件（用于下载图片时使用）
+    '''
+    # 分类 性感、日本、台湾、清纯 要爬那哪个类型就选哪个
+    index_urls = ['https://www.mzitu.com/xinggan/','https://www.mzitu.com/japan/','https://www.mzitu.com/taiwan/','https://www.mzitu.com/mm/']
+    # 此处选的是性感类型
+    index_url = 'https://www.mzitu.com/xinggan/'
+    # 用于保存所有套图名称及url地址
+    all_img_info = []
+    # 1.获取首页response响应内容
+    response = get_response(index_url)
+    # 2.解析内容获取套图名称及url地址
+    all_img_info.extend(parse_html1(response))
+    # 3.获取下一页url地址
+    next_page_url = get_next_page_url(response)
+    # 4.获取下一页response响应内容 解析内容获取套图名称及url地址
+    while next_page_url:
+        print(next_page_url)
+        # 设置延时 2秒 防止请求过于频繁
+        sleep(2)
+        # 1.获取response响应内容
+        response = get_response(next_page_url)
+        # 2.解析内容获取套图名称及url地址
+        all_img_info.extend(parse_html1(response))
+        # 3.获取下一页url地址
+        next_page_url = get_next_page_url(response)
+    print('爬取完成，供%d条数据'%len(all_img_info))
+    # 5.保存到json文件
+    # 注意：如果要爬不同类型的要修改filename变量，防止覆盖掉原来保存的json文件
+    save_to_file(filename='meizi',data=all_img_info)
+
+
+if __name__ == '__main__':
+    run()
