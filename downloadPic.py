8acfc1f5a8529b448c4a4d15319bdca37ad365d0
master <1649601714@qq.com>
2019-04-12 13:40:55 +0800

    Update downloadPic.py

--- downloadPic.py
+++ downloadPic.py
@@ -11,6 +11,19 @@ def read_json_file(filename):
         data = json.loads(data)
     return data
 
+def judge_file(filename):
+    '''判断文件是否存在
+    存在返回True
+    不存在返回False
+    '''
+    try:
+        with open(filename) as f:
+            pass
+        return True
+    except:
+        return False
+
+
 def create_dictionary(name):
     '''创建文件夹'''
     try:
@@ -25,13 +38,20 @@ def download_picture(name,url):
         'Referer': 'https://www.mzitu.com/',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
     }
-    img_content = get_response(url,headers=headers).content
-    try:
-        print('Pictures/'+name+'/'+name+url[-8:])
-        with open('Pictures/'+name+'/'+name+url[-8:],'wb') as f:
-            f.write(img_content)
-    except Exception as e:
-        print(e)
+    filename = 'Pictures/'+name+'/'+name+url[-8:]
+    judge = judge_file(filename)
+    if judge:
+        print('文件{}已存在'.format(filename))
+    else:
+        img_content = get_response(url,headers=headers).content
+        try:
+            print(filename)
+            with open(filename,'wb') as f:
+                f.write(img_content)
+                # 设置延时
+                sleep(1)
+        except Exception as e:
+            print(e)
 
 def get_picture_url(name,url):
     '''获取图片'''
@@ -50,8 +70,6 @@ def get_picture_url(name,url):
             html = etree.HTML(response.text)
             img_url = html.xpath('//div[@class="main-image"]//img/@src')[0]
             download_picture(name, url=img_url)
-            # 设置延时
-            sleep(1)
             # 获取下一页
             next_page_url = html.xpath('//a[./span/text()="下一页»"]/@href')[0]
     except Exception as e:
